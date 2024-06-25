c3bcbf1aa8c38ff0bc1593b5b5093f9eb54f4daa
Mattei Mathieu <mattei.mathieu2b1@gmail.com>
2024-06-25 20:22:43 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <HelloWorld :user="user" />
+
+  <input type="text" v-model="user.name" @change="display" />
+  <input type="text" v-model="user.age" />
+</template>
+
+<script setup>
+import HelloWorld from "@/components/HelloWorld.vue";
+import { reactive } from "vue";
+
+const user = reactive({
+  name: "NordCoders",
+  age: 30,
+});
+
+const display = () => {
+  console.log(user.name, user.age);
+};
+
+display();
+</script>
+
+<style scoped></style>
